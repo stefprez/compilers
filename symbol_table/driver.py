431474104e28561be68bd316fee946cb4eb69d39
Stefano Prezioso <stefprez@gmail.com>
2016-04-20 08:45:52 -0600

    Started driver and parser

---
+++ symbol_table/driver.py
@@ -0,0 +1,13 @@
+# Set up command line arguments
+
+# Read in file
+
+# Parse lines
+
+# Create symbol table objects
+
+# Add to symbol table
+
+# Continue to end of file
+
+# Print out Symbol Table
---
+++ symbol_table/parser.py
@@ -0,0 +1,10 @@
+
+class Parser:
+    keywords = set(["auto", "break", "case", "char", "const", "continue",
+                    "default", "do", "double", "else", "enum", "extern",
+                    "float", "for", "goto", "if", "int", "long", "register",
+                    "return", "short", "signed", "sizeof", "static", "struct",
+                    "switch", "typedef", "union", "unsigned", "void",
+                    "volatile", "while"])
+
+    def __init__(self):

320b5f82718543065d3facc15f14aa74aaf7dca9
Stefano Prezioso <stefano.prezioso@sendgrid.com>
2016-04-24 16:11:53 -0600

    File for testing

---
+++ symbol_table/test.c
@@ -0,0 +1,29 @@
+static int w;
+char x;
+float b[10], c;
+int main() {
+  int * c = 2;
+  {
+    static int **b[2][3], z;
+  }
+  {
+    int a, x;
+    {
+      double c, x;
+      int d;
+      int j, i;
+      x = a - a + c / x * 2 % 3 % x + 3;
+      int my_int = 3, other = 4;
+
+      j = my_int;
+    }
+  }
+}
+
+void example2(int x, int y) {
+  int a = 9;
+}
+
+void example3(int a, char b, int c) {
+  int a = 9;
+}
